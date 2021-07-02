c9b9f3d52c9d90af29fe9bc047f9d480dadd0aa7
Jessica Jennings <75150881+hey-jessica-j@users.noreply.github.com>
2021-07-02 14:38:38 -0400

    added facts for math, quotes, and date

--- js/script.js
+++ js/script.js
@@ -4,12 +4,21 @@
 
 //Global variables
 const dogElement = document.querySelector(".dog");
-const todaysDateElement = document.querySelector(".todays-date");
-const mathElement = document.querySelector(".math-class");
-const passwordElement = document.querySelector(".password");
-const quoteElement = document.querySelector(".quote");
+const containerElement = document.querySelector(".container");
 const catElement = document.querySelector(".cat");
 
+
+//Display facts function
+const displayFact = async function(title, classE, fact) {
+  const div = document.createElement("div");
+  div.classList.add(classE);
+  div.innerHTML =
+  `
+  <h3> ${title}</h3>
+  <p> ${fact} </p>
+  `;
+  containerElement.append(div);
+};
 
 //Takes Todays date and generates a fact about it
 //Possible improvements - add a timer so it creates a new fact after a cetain time passes
@@ -19,15 +28,36 @@ const dateFact = async function() {
   const dd = today.getDate();
   const mm = today.getMonth() + 1;
   const yy = today.getYear();
+
+  let title = `On today's date: ${mm}/${dd}...`;
+  const classE = "todays-date";
+
   const fetchData = await fetch(`http://numbersapi.com/${mm}/${dd}/date`);
   const dateFact = await fetchData.text();
-  displayDateFact(dateFact);
+  displayFact(title, classE, dateFact);
 };
-//Creates a place for fact in document and shows the element
-const displayDateFact = async function(dateFact) {
-  todaysDateElement.classList.remove("hide");
-  todaysDateElement.innerHTML =
-  `<p> ${dateFact}</p>`;
+
+//Taks a random number ande generates a fact about it
+//Grabs the math API
+const numberFact = async function() {
+  const title = "Random Math Fact:"
+  const classE = "math-class";
+  const fetchData = await fetch("http://numbersapi.com/random/math");
+  const mathFact = await fetchData.text();
+  displayFact(title, classE, mathFact);
+};
+
+const quoteFact = async function() {
+  const title = "Random quote:"
+  const classE = "quote";
+  const fetchData = await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random");
+  const quoteData = await fetchData.json();
+  const quote = " 	&quot; " + quoteData.data[0].quoteText + " 	&quot; &#126; " + quoteData.data[0].quoteAuthor;
+  //displayFact(quoteElement, classE, quote);
+  displayFact(title, classE, quote);
+
 };
 
 dateFact();
+numberFact();
+quoteFact();
